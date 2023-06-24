b2e0028fc5e15f3ab3df39f7272df0dbf1df8828
Akash Kumar <akash211@gmail.com>
2023-06-24 12:17:38 +0530

    files rearranged

--- 1Data Structures/chapter_2/chapter_2_Arrays.py
+++ 1Data Structures/chapter_2/chapter_2_Arrays.py
@@ -11,12 +11,12 @@ floats = array('d', (random() for _ in range(10**7)))
 
 print(floats[-1])
 
-fp = open('floats.bin', 'wb')
+fp = open('../floats.bin', 'wb')
 floats.tofile(fp)
 fp.close()
 
 floats2 = array('d')
-fp = open('floats.bin', 'rb')
+fp = open('../floats.bin', 'rb')
 floats2.fromfile(fp, 10**7)
 fp.close()
 print(floats2[-1])
