1a5085466d16a643e710d9e1f2f503e7cf9bed7f
Akash Kumar <akash211@gmail.com>
2023-06-29 20:28:17 +0530

    Update encoding_decoding.py

---
+++ 1Data Structures/Chapter_4/encoding_decoding.py
@@ -0,0 +1,8 @@
+# python has more than 100 codecs
+# These can be used as encoding argument to  open(), str.encode(), bytes.decode()
+
+# common codecs:
+# 1. latin1/iso8859_1 : basis for other encodings.
+# 2. cp1252 : latin1 superset created by Microsoft which added many symbols. (someplace it is called ANSI, but it is not really ANSI)
+# 3. utf-8 : most popular 8-bit encoding. 97% websites uses this.
+# 4. utf-16-le (little-endian): UTF 16 bit encoding scheme type which support code points beyond U+FFFF through escape sequences called surrogate pairs
